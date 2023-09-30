f0dcc0fa361f182ed6eb0fe607eab543ed551468
Vishal <vishal.manglani7@gmail.com>
2023-09-30 14:28:38 -0400

    created template for info that will go in readme

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 const inquirer = require ("inquirer");
 const fs = require ("fs");
 
-//cont generateReadme
+const generateReadme = (obj) =>{
+    const { name, description, installation, usage, contribution, test, license, email} = obj;
+
+    return ` <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>${name}</title>
+    </head>
+    
+    <body>
+        <h1>${name}</h1>
+    
+        <h2>Description</h2>
+        <p>${description}</p>
+    
+        <h2>Installation</h2>
+        <p>${installation}</p>
+    
+        <h2>Usage</h2>
+        <p>${usage}</p>
+    
+        <h2>Contribution</h2>
+        <p>${contribution}</p>
+    
+        <h2>Tests</h2>
+        <p>${test}</p>
+    
+        <h2>License</h2>
+        <p>${license}</p>
+    
+        <p>For questions please contact: ${email}</p>
+    
+    </body>
+    
+    </html>`;}
+
 
 inquirer
     .prompt([
@@ -18,7 +57,7 @@ inquirer
         message: "Enter installation instructions",
         },
         {type: "input",
-        name: "useage",
+        name: "usage",
         message: "Enter useage info",
         },
         {type: "input",
